971f324d427811b780c0aa65c1eb543f92c52cc2
marcobosc <marcob.brl@gmail.com>
2024-06-23 19:14:07 -0300

    Navbar criada

--- FrontEnd/LivrosAutores/src/App.jsx
+++ FrontEnd/LivrosAutores/src/App.jsx
@@ -1,12 +1,23 @@
-import './App.css'
+import './App.module.css'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
+import Navbar from '../Components/Navbar';
+import Livros from '../Components/Livros';
+import Autores from '../Components/Autores';
 
 function App() {
 
   return (
-    <>
-      <h1>Livros e Autores</h1>
-    </>
-  )
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/livros" element={<Livros />} />
+          <Route path="/autores" element={<Autores />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App
