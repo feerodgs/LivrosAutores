bbc3c5806afa61af3209372c5622f1097ecb5c57
Felipe Rodrigues <58119435+feerodgs@users.noreply.github.com>
2024-06-23 18:38:09 -0300

    Create files

---
+++ BackEnd/src/utils/database.js
@@ -0,0 +1,28 @@
+import mysql from 'mysql2/promise';
+
+//const connectionConfig = {
+//    host: 'localhost',
+//    user: 'test',
+//    password: 'masterkey',
+ //   database: 'utrack',
+ //   connectTimeout: 900000000
+//};
+
+ const connectionConfig = {
+     host: 'localhost',
+     user: 'root',
+     password: 'masterkey',
+     database: 'utrack',
+     connectTimeout: 900000000
+ };
+
+export async function connectToDatabase () {
+    try {
+        const connection = await mysql.createConnection(connectionConfig);
+        console.log('Conexão com o banco de dados estabelecida com sucesso! 1');
+        return connection;
+    } catch (error) {
+        console.error('Erro ao conectar com o banco de dados:', error);
+        throw error;
+    }
+};
